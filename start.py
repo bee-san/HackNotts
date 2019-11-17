e25ba2ff2f9832e8f8395cec732e5d051038ef09
unknown <G.Rowley@ncl.ac.uk>
2019-11-17 03:23:50 +0000

    kek

--- start.py
+++ start.py
@@ -1,13 +1,16 @@
 from telegram.ext import MessageHandler, Filters 
 from telegram.ext import Updater, CommandHandler
 import requests
-updater = Updater(token='1019752473:AAEzlIeStcZB25djykEjNJLQzVlyVn8p7YU', use_context=True)
+import telegramToken
+
+updater = Updater(token= telegramToken.token, use_context=True)
+wit = "https://api.wit.ai/message"
 
 
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm brandonBot.")
 
 def get_url():
     url = requests.request("GET", "http://api.giphy.com/v1/gifs/random", params={"api_key":"pgHiOtZymIiPTrEgdYWqqYav1eSjOIgR","rating":"PG-13"}).json()
@@ -19,8 +22,18 @@ def photo(update, context):
     context.bot.send_animation(chat_id=chat_id, animation=url)
     
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
+    txt = update.message.text
+    querystring = {"v":"20191117","q":txt}
+    
+    headers = {
+        'Authorization': "Bearer O22LR566VRN74YN7FLOVI3NADYISG4SO",
+        'cache-control': "no-cache",
+        'Postman-Token': "8dc65357-32b6-4252-81ec-224fc7a1165e"
+    }
+    response = requests.request("GET", wit, headers=headers, params=querystring).json()
+    print(response['entities']['intent'][0]['value'])    
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response.text)
     
 def getChatID(update, context):
     #context.bot.send_message(chat_id=update.effective_chat.id, text="I'm another bot, please talk to me!")
