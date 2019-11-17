2661947cda46918ae0c753bb7dd5b6fcea5a5ab8
unknown <G.Rowley@ncl.ac.uk>
2019-11-17 09:43:44 +0000

    kek

--- start.py
+++ start.py
@@ -12,13 +12,12 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm brandonBot.")
 
-def get_url():
-    url = requests.request("GET", "http://api.giphy.com/v1/gifs/random", params={"api_key":"pgHiOtZymIiPTrEgdYWqqYav1eSjOIgR","rating":"PG-13"}).json()
-    print(url)
+def get_url(tag):
+    url = requests.request("GET", "http://api.giphy.com/v1/gifs/random", params={"api_key":"pgHiOtZymIiPTrEgdYWqqYav1eSjOIgR","rating":"PG-13", "tag":{tag, "money"}}).json()
     return url['data']['images']['original']['url']
 
-def gif(update, context):
-    url = get_url()
+def gif(update, context, tag):
+    url = get_url(tag)
     chat_id = update.effective_chat.id
     context.bot.send_animation(chat_id=chat_id, animation=url)
   
@@ -32,42 +31,32 @@ def get_meaning(update, context):
         'Postman-Token': "8dc65357-32b6-4252-81ec-224fc7a1165e"
     }
     response = requests.request("GET", wit, headers=headers, params=querystring).json()
-    #print(response);
-    print("THIS")
-    print(next(iter(response['entities'])))
-        
     return next(iter(response['entities']))
     
 def echo(update, context):
-    #context.bot.send_message(chat_id=update.effective_chat.id, text="AAAHH")
     meaning = get_meaning(update, context)
-    #print(intent)
+    print(meaning)
     if (meaning == "intent"):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=banking.get_balance(banking.uid))
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Your balance is " + str(banking.get_balance(banking.uid)))
     if (meaning == "average_paying"):
         result = banking.assess_last_transaction(banking.uid, banking.category, banking.last)
-        #if (result[1] == "IN"):
-            
-        #else:
-            
-        #print(result[0])
+        if (result[1] == "IN"):
+            gif(update, context, "good")
+        else:
+            gif(update, context, "bad")
         context.bot.send_message(chat_id=update.effective_chat.id, text=banking.assess_last_transaction(banking.uid, banking.category, banking.last))
     if (meaning == "last_transaction"):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=banking.get_last_transaction_from_feed((banking.uid, banking.category)))
-        
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=intent)
-    
-#def getChatID(update, context):
-    ##context.bot.send_message(chat_id=update.effective_chat.id, text="I'm another bot, please talk to me!")
-    #chat_id = update.effective_chat.id
-    #txt = f"The exact chat ID is \"{chat_id}\" and the type is {type(chat_id)}"
-    #context.bot.send_message(chat_id=chat_id, text=txt)
+        result = banking.get_last_transaction_from_feed(banking.uid, banking.category)
+        if (result["direction"] == "IN"):
+            gif(update, context, "good")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Your last transaction was inbound, whoooo!")
+        else:
+            gif(update, context, "bad")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Your last transaction was outbound. Crap.")
 
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-#chat_handler = CommandHandler('chat', getChatID)
-#dispatcher.add_handler(chat_handler)
 photo_handler = CommandHandler('gif', gif)
 dispatcher.add_handler(photo_handler)
 echo_handler = MessageHandler(Filters.text, echo)
